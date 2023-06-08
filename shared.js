e275ad166f3b63323e9ea89f8df38a630bb31420
Matt Bilyeu <matt.bilyeu1@gmail.com>
2023-06-08 13:34:47 -0400

    Finished Styling Existing Pages

--- shared.js
+++ shared.js
@@ -6,3 +6,26 @@ hamburger.onclick = function() {
     const mobileNav = document.querySelector('.mobileNav');
     mobileNav.style.display = 'flex';
 }
+
+function getFileNameFromURL() {
+    const path = new URL(window.location.href).pathname;
+    const fileName = path.substring(path.lastIndexOf('/') + 1);
+    return fileName;
+}
+
+function updateSMSIconPosition() {
+    const pageWidth = document.documentElement.clientWidth;
+    let maxPageWidth = 1200;
+    const fileName = getFileNameFromURL();
+    if(fileName === 'speakers.html') {
+        maxPageWidth = 750;
+    } else if(fileName === 'contact-us.html') {
+        return false;
+    };
+    const offset = (pageWidth - maxPageWidth)/2 + 1;
+    const smsIcon = document.querySelector('.smsIcon');
+    smsIcon.style.right = offset + 'px';
+}
+
+updateSMSIconPosition();
+window.addEventListener('resize', updateSMSIconPosition);
